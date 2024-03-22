70e426a0096cb745835757d395bbcd02639b8f5f
HARSHIL SRIVASTAVA <34956748+HarshilSrivastava@users.noreply.github.com>
2024-03-22 08:33:22 +0000

    exploring variables in JS

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 23454;
+var accountPassword = "Hello";
+let accountName = "Anonymous";
+accountPlace = "Jaipur";
+
+console.table([accountId, accountName, accountPassword,accountPlace]);
